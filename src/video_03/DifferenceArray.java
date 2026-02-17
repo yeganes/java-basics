33239263cdbbe8de990c177e1a5121368fcd2ec7
Yegane <yeganehsalimi42@gmail.com>
2026-02-17 13:48:26 +0330

    مقایسه دو ارایه ایندکس به ایندکس

---
+++ src/video_03/DifferenceArray.java
@@ -0,0 +1,47 @@
+package video_03;
+import java.util.Arrays;
+import java.util.Scanner;
+public class DifferenceArray {
+    public static void main(String[] args){
+
+
+
+        Scanner input = new Scanner(System.in);
+
+        int [] array1 = new int[3];
+        int [] array2 = new int[3];
+
+        for(int i  = 0 ; i<3 ; i++ ){
+            int j = i + 1;
+           System.out.println("give your first array's number element  " +   j + " : ");
+           array1[i] = input.nextInt();
+
+        }
+        for(int i  = 0 ; i<3 ; i++ ){
+            int j = i + 1;
+            System.out.println("give your second array's number : " +   j + " : ");
+            array2[i] = input.nextInt();
+        }
+
+        System.out.println("your first array is : " + Arrays.toString(array1));
+        System.out.println("your second array is : " + Arrays.toString(array2));
+        for(int i = 0 ; i <3 ; i++ ){
+            if (array1[i] == array2[i]) {
+                int j = i + 1;
+                System.out.println("the element number " + j +"  are the same");
+            }else{
+                i++;
+                }
+        }
+        }
+
+
+
+
+
+
+
+
+
+    }
+
