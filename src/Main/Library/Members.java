ecbfff018f548406b5af32e6f51102a7ed7cf18d
yegane <yeganehsalimi42@gmail.com>
2026-04-12 11:02:40 +0330

    Add Car.java

--- src/Main/Library/Members.java
+++ src/Main/Library/Members.java
@@ -1,8 +1,5 @@
 package Main.Library;
-import java.util.Scanner;
 public class Member {
-
-    static Scanner input = new Scanner(System.in);
 
     private String name;
     private int age;
@@ -26,9 +23,6 @@ public class Member {
     }
 
     public int getAge() {
-        if (age > 100 || age < 5) {
-            return 404;
-        }
         return age;
     }
 
@@ -66,35 +60,10 @@ public class Member {
         this.phone = phone;
         this.idMembership = idMembership;
     }
+}
     //اماده سازی اولیه اشیا
 
-    public static void main(String[] args) {
-        Member person = null;
-        //شیء پرسن از کلاس ممبر به صورت دیفالت نال یکبار تعریف میکنیم
-            System.out.println("Welcome to the library ");
-            System.out.println("Would you please enter your name please : ");
-            String getUserName = input.nextLine();
-            System.out.println("Now please enter your age please : ");
-            int getUserAge = input.nextInt();
-            input.nextLine();
-            System.out.println("Please choose your gender M(male) , F(female) : ");
-            char getUserGender = input.next().toUpperCase().charAt(0);
-            System.out.println("Please enter your phone number :+98 ");
-            long getUserphone = input.nextLong();
-            input.nextLine();
-            long setUserMembershipId = 404_000_000_000_000L + getUserphone;
 
-
-            person = new Member(getUserName, getUserAge, getUserGender, getUserphone, setUserMembershipId);
-            System.out.println("your name is : " + person.getName());
-            System.out.println("your are  : " + person.getAge() + " years old");
-            System.out.println(person.getGender());
-            System.out.println("your phone number is : " + person.getPhone());
-            System.out.println("your ID is : " + person.getIdMembership());
-            String[] array = new String[4];
-            array[1] = person.getName();
-        }
-    }
 
 
 
