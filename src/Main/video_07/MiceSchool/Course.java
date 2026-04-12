53b379863e25927752644f557589881c858fe4f2
yegane <yeganehsalimi42@gmail.com>
2026-04-12 14:33:55 +0330

    "adding course's name"

--- src/Main/video_07/MiceSchool/Course.java
+++ src/Main/video_07/MiceSchool/Course.java
@@ -2,6 +2,18 @@ package Main.video_07.MiceSchool;
 import java.util.Arrays;
 import java.util.Scanner;
 public class Course{
+    private String name;
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public Course (String name){
+        this.name = name ;
+    }
+    Course course = new Course("Math");
+
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
